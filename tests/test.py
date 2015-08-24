89471bf1e1dd7c89573c112671066fea0713ec95
Anton Shekhov <sh.anton2111@gmail.com>
2015-08-24 10:53:55 +0200

    Tests for file list were created

--- tests/test.py
+++ tests/test.py
@@ -10,9 +10,6 @@ from src import fastqc_reports
 
 class FastqcTestCase (unittest.TestCase):
         """ Test of the fastqc report merging tool"""
-        def testInputZero (self):
-                """ Should give a message about usage """
-
         @unittest.skip
         def testHelpCalled (self):
                 """ Test Error. Argument -h or help should cause System exit """
@@ -28,11 +25,28 @@ class FastqcTestCase (unittest.TestCase):
                 path = os.path.join (os.getcwd(), test_path)
                 test_result = fastqc_reports.input_handler (("-i", test_path))
                 self.assertEqual (test_result['i'], path)
+                self.assertEqual (test_result['o'], path)                
 
-        def testZipFiles (self):
-                """Test Error. Directory should contain at least one .zip file if -z argument was passed"""
+        def testListFiles (self):
+                """Given folder should contain needable files"""
                 # Test works only if calles from parent directory
-                self.assertRaises(fastqc_reports.TypeError, fastqc_reports.test_files, ("-z", "-i", "src"))
+                path = os.getcwd()
+                no_files = os.path.join (path, "tests/no_files")
+                zip_files = os.path.join (path, "tests/zip_files")
+                folder_files = os.path.join (path, "tests/report_folders")
+                
+                # Error if empty folder is given
+                self.assertRaises (fastqc_reports.TypeError, fastqc_reports.get_file_list, no_files, False)
+                # Error if folder with no report .zip files
+                self.assertRaises(fastqc_reports.TypeError, fastqc_reports.get_file_list, folder_files, True)
+                # Error if folder without report folders
+                self.assertRaises(fastqc_reports.TypeError, fastqc_reports.get_file_list, zip_files, False)
+        
+        def testZipRecognition (self):
+                # Test is returning TRUE when -z parameter is given, and FALSE when not
+                test_result = fastqc_reports.input_handler (('z'))
+                self.assertTrue (test_result['z'])
+                self.assertFasle (fastqc_reports.input_handler()['z'])
 
         def testWrongFileExtention (self):
                 """Test Error. Given name should be with .csv extention"""
