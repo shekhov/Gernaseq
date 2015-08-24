cd90ab8f4ddbfeb4c26da70232defc8d598a21bd
Anton Shekhov <sh.anton2111@gmail.com>
2015-08-24 10:08:44 +0200

    Change tabs to 8*spaces

--- tests/test.py
+++ tests/test.py
@@ -9,34 +9,34 @@ sys.path.append  (os.path.abspath("./"))
 from src import fastqc_reports
 
 class FastqcTestCase (unittest.TestCase):
-    """ Test of the fastqc report merging tool"""
-    def testInputZero (self):
-        """ Should give a message about usage """
+        """ Test of the fastqc report merging tool"""
+        def testInputZero (self):
+                """ Should give a message about usage """
 
-    @unittest.skip
-    def testHelpCalled (self):
-        """ Test Error. Argument -h or help should cause System exit """
-        self.assertRaises(SystemExit, fastqc_reports.input_handler, ("-h", ""))
-        self.assertRaises(SystemExit, fastqc_reports.input_handler, ("--help", ""))
+        @unittest.skip
+        def testHelpCalled (self):
+                """ Test Error. Argument -h or help should cause System exit """
+                self.assertRaises(SystemExit, fastqc_reports.input_handler, ("-h", ""))
+                self.assertRaises(SystemExit, fastqc_reports.input_handler, ("--help", ""))
 
-    def testInputPath (self):
-        """Tests for input folder"""
-		# Should give an error if not exit
-        self.assertRaises(fastqc_reports.InputError, fastqc_reports.input_handler, ("-i", "poo"))
-		# Test works only if calles from parent directory
-		test_path = "src"
-		path = os.path.join (sys.getcwd(), test_path)
-		test_result = fastqc_reports.input_handler ("-i", test_path)
-		self.assertEqual (test_result['i'] = path)
+        def testInputPath (self):
+                """Tests for input folder"""
+                # Should give an error if not exit
+                self.assertRaises(fastqc_reports.InputError, fastqc_reports.input_handler, ("-i", "poo"))
+                # Test works only if calles from parent directory
+                test_path = "src"
+                path = os.path.join (sys.getcwd(), test_path)
+                test_result = fastqc_reports.input_handler ("-i", test_path)
+                self.assertEqual (test_result['i'] == path)
 
-    def testZipFiles (self):
-        """Test Error. Directory should contain at least one .zip file if -z argument was passed"""
-		# Test works only if calles from parent directory
-        self.assertRaises(fastqc_reports.TypeError, fastqc_reports.test_files, ("-z", "-i", "src"))
+        def testZipFiles (self):
+                """Test Error. Directory should contain at least one .zip file if -z argument was passed"""
+                # Test works only if calles from parent directory
+                self.assertRaises(fastqc_reports.TypeError, fastqc_reports.test_files, ("-z", "-i", "src"))
 
-    def testWrongFileExtention (self):
-        """Test Error. Given name should be with .csv extention"""
-        self.assertRaises(fastqc_reports.InputError, fastqc_reports.input_handler, ("-f", "poo.bar"))
+        def testWrongFileExtention (self):
+                """Test Error. Given name should be with .csv extention"""
+                self.assertRaises(fastqc_reports.InputError, fastqc_reports.input_handler, ("-f", "poo.bar"))
 
 if __name__ == '__main__':
-    unittest.main()
+        unittest.main()
