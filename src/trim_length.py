a88b69e241794ef13479a0a1476c87b243ca75a4
Anton Shekhov <sh.anton2111@gmail.com>
2015-10-18 14:56:35 +0200

    Change argument parsing

--- src/trim_length.py
+++ src/trim_length.py
@@ -1,7 +1,8 @@
 import argparse
 import sys
 
-def trimming (input_path, start, end, output_path):
+def trimming (input_path, output_path, start, end, keep):
+
         input_file = open (input_path, 'r')
         output_file1 = open (output_path, "w")
 
@@ -11,6 +12,8 @@ def trimming (input_path, start, end, output_path):
         nseq = (len(data)-1)/2
         print ("Initial number of sequences were = ", nseq)
         count = 0
+        keep_count = 0
+
         for i in range (len(data)-1):
                 f = data[i] # name
                 s = data[i+1] # sequence
@@ -18,8 +21,14 @@ def trimming (input_path, start, end, output_path):
                         if len (s) >= end:
                                 temp_s = s[start:end]   
                                 output_data[f] = temp_s
-                        else : count += 1
+                        else :
+                                count += 1
+                                if keep:
+                                        output_data[f] = s
+                                        keep_count += 1
+
         print (count , " sequences were less than ", end-start)
+        if keep: print (keep_count, " sequences were kept, regardless their small length")
         print ("Final number of sequences is ", nseq-count)
 
         print ("Writing output file...")    
@@ -36,23 +45,29 @@ def trimming (input_path, start, end, output_path):
 def InputHandler (args):
         parser = argparse.ArgumentParser(description='Trim sequences to achieve desire length regardless to the quality'
                                                      'If the initial sequence line is less than END-START, do not include'
-                                                     ' it into the outcome file.'
+                                                     ' it into the outcome file. '
                                                      'This can be changed by setting flag --keep')
-        parser.add_argument ("-i", "--input", metavar="INPUT", default="input.fasta",
-                        help="Path to the sequence file in fasta format")
+        parser.add_argument ("input", metavar="input.fasta", type=argparse.FileType('r'),
+                             nargs="?", default="input.fasta",
+                             help="Path to the sequence file in fasta format")
+        parser.add_argument ("output", metavar =  "output.fasta", default="output.fasta",
+                     type=argparse.FileType('w'),
+                     help="Path to the output file with trimming sequences")
+        parser.add_argument ("end", metavar="END", type=int,
+                     help="The position of the right border in the frame. "
+                          "When START is 0 (default), END is also a length of the frame")
+
         parser.add_argument ("-s", "--start", metavar="START", default=0, type=int,
-                        help="The position of the left border in the frame")
-        parser.add_argument ("-e", "--end", metavar="END", type=int, 
-                        help="The position of the right border in the frame")
-        parser.add_argument ("-o", "--output", metavar =  "OUTPUT", default="output.fasta",
-                        help="Path to the output file with trimming sequences")
+                             help="The position of the left border in the frame (default: %(default)s)")
+        parser.add_argument ("--keep", action="store_true",
+                             help="Should the frames less than END-START be kept in the output file (default: %(default)s)")
+        parser.add_argument ("--version", action='version', version='%(prog)s 0.1')
                         
         return (parser.parse_args())
         
-        
 def main (argv):
         input = InputHandler (argv)
-        trimming (input.input, input.start, input.end, input.output)
+        trimming (input.input, input.output, input.start, input.end, input.keep)
         
 if __name__== "__main__":
         main (sys.argv[1:])
