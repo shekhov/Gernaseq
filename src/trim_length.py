7f2391b43bbbf2dd9e9eafdfe5c0e6dff0f61c6e
Anton Shekhov <sh.anton2111@gmail.com>
2015-10-18 18:29:08 +0200

    New test

--- src/trim_length.py
+++ src/trim_length.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-def trimming (input_path, output_path, start, end, keep):
+def trimming (input_path, output_path, end, start=0, keep=False, quiet=False):
 
         input_file = open (input_path, 'r')
         output_file1 = open (output_path, "w")
@@ -10,7 +10,7 @@ def trimming (input_path, output_path, start, end, keep):
 
         data = input_file.read().split ("\n")
         nseq = (len(data)-1)/2
-        print ("Initial number of sequences were = ", nseq)
+        if not quiet: print ("Initial number of sequences were = ", nseq)
         count = 0
         keep_count = 0
 
@@ -26,24 +26,23 @@ def trimming (input_path, output_path, start, end, keep):
                                 if keep:
                                         output_data[f] = s
                                         keep_count += 1
+        if not quiet:
+                print (count , " sequences were less than ", end-start)
+                if keep: print (keep_count, " sequences were kept, regardless their small length")
+                print ("Final number of sequences is ", nseq-count)
 
-        print (count , " sequences were less than ", end-start)
-        if keep: print (keep_count, " sequences were kept, regardless their small length")
-        print ("Final number of sequences is ", nseq-count)
-
-        print ("Writing output file...")    
+                print ("Writing output file...")
         for names, seq in output_data.items():
                 output_file1.write (names + '\n')
                 output_file1.write (seq + "\n")
 
         input_file.close()
         output_file1.close()
-
-        print ("Done")
-        input ("Enter to exit")     
+        if not quiet:
+                print ("Done")
+                input ("Enter to exit")
         
 def InputHandler (args):
-        print (args)
         parser = argparse.ArgumentParser(description='Trim sequences to achieve desire length regardless to the quality'
                                                      'If the initial sequence line is less than END-START, do not include'
                                                      ' it into the outcome file. '
@@ -56,19 +55,21 @@ def InputHandler (args):
                              help="Path to the output file with trimming sequences")
         parser.add_argument ("end", metavar="END", type=int, nargs='?',
                              help="The position of the right border in the frame. "
-                                  "When START is 0 (default), END is also a length of the frame")
+                                  "When START is 0 (default), END is also a length of the frames")
 
         parser.add_argument ("-s", "--start", metavar="START", default=0, type=int,
                              help="The position of the left border in the frame (default: %(default)s)")
         parser.add_argument ("--keep", action="store_true",
                              help="Should the frames less than END-START be kept in the output file (default: %(default)s)")
         parser.add_argument ("--version", action='version', version='Script %(prog)s v0.1')
+        parser.add_argument ("-q", "--quiet", action="store_false",
+                             help="Do not produce output")
                         
         return (parser.parse_args(args))
         
 def main (argv):
         input = InputHandler (argv)
-        trimming (input.input, input.output, input.start, input.end, input.keep)
+        trimming (input.input, input.output, input.end, input.start,input.keep, input.quiet)
         
 if __name__== "__main__":
         main (sys.argv[1:])
--- tests/_trim_length_test.py
+++ tests/_trim_length_test.py
@@ -13,16 +13,32 @@ class TrimLengthCase (unittest.TestCase):
         def setUp(self):
                 self.input = os.path.join("tests", "testing_files", "trim_length.fasta")
                 self.output = os.path.join("tests", "testing_files", "temp.fasta")
+                self.end = 20
+                self.start = 2
+                self.first_0_20 = "AAAAAAAAAAAAAAAAAAAA"
+                self.first_2_20 = "AAAAAAAAAAAAAAAAAAAA"
+                self.second_0_20 = "TTTTCTTTTTTTTTTTTTTT"
+                self.second_2_20 = "TTCTTTTTTTTTTTTTTTTT"
+                self.third_0_20_keep = "ATACTA"
+                self.third_2_20_keep = "ATACTA"
         def testInputHandler (self):
                 """
                 Check returning arguments
                 """
                 args = [self.input, self.output , '20']
-                #args = ['tests\\testing_files\\trim_length.fasta', 'tests\\testing_files\\temp.fasta', '20']
                 result = trim_length.InputHandler(args)
+
                 self.assertEqual (result.input, self.input)
                 self.assertEqual (result.output, self.output)
                 self.assertEqual (result.end, 20)
+
+        def testOutputFile (self):
+                """
+                Check correct output
+                """
+                args_first = [self.input, self.output, self.end]
+                args_second = [self.input, self.output, self.end, "-s", self.start]
+                first_trim = trim_length.trimming(self.input, self.output, self.end, quiet=True)
 
 
 
