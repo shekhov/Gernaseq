cf7df64e341cb397274131dcf9b74697a7c37bd6
Anton Shekhov <sh.anton2111@gmail.com>
2015-10-19 14:50:23 +0200

    Before merging with master

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,7 @@ import os.path
 # Spend two hours to figure this shit out!!!
 sys.path.append  (os.path.abspath("./"))
 from src import fastqc_reports
+from src import combine_velvet_reports
 
 class FastqcTestCase (unittest.TestCase):
         """ Test of the fastqc report merging tool"""
@@ -121,7 +122,24 @@ class FastqcTestCase (unittest.TestCase):
                 #TODO: Add more tests to this section
 
 
-
+class VelvetTestCase (unittest.TestCase):
+        def testInputErrors (self):
+                pass
+                
+        def testRightInput (self):
+                i_path = os.getcwd()
+                o_path = os.getcwd()
+                r_test = [1,5]
+                f_test = "velvet_report_1-5.csv"
+                test_result = combine_velvet_reports.input_handler (("-i", i_path, '-r', '1-5'))
+                self.assertEqual (i_path, test_result['i'])
+                self.assertEqual (o_path, test_result['o'])
+                self.assertEqual (f_test, test_result['f'])
+                self.assertEqual (r_test, test_result['r'])
+                
+        def testWritReport (self):
+                pass
+        
 
 if __name__ == '__main__':
         unittest.main()
