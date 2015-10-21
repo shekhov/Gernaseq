399655df9639ef1e32785e5d0a473eb2753e764d
Anton Shekhov <sh.anton2111@gmail.com>
2015-10-21 12:34:24 +0200

    Separate methods and new tests

--- src/combine_velvet_reports.py
+++ src/combine_velvet_reports.py
@@ -78,31 +78,48 @@ def input_handler2 (argv):
         return {'i':inputFolder, 'o':outputFolder, 'f':outputFile, 'r':rangeN}
 
 def input_handler (args):
+        def range_numbers (string):
+                #print (string + "pOOOO")
+                if '-' not in string:
+                        msg = "Range does not have - sign"
+                        raise argparse.ArgumentTypeError (msg)
+                else:
+                        result = [int(x) for x in string.split("-")]
+                        if len (result) != 2:
+                                msg = "Wrong range format was given"
+                                raise argparse.ArgumentTypeError (msg)
+                        return result
+                        
+        
+                        
         parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                          description='Combine reports from velvet assembly to a single .csv table')
-                                         
-        parser.add_argument ("input", metavar="INPUT_FOLDER", nargs="?", default=os.getcwd(), help = "Path to the folder where assemblies are located")
-                
-        parser.add_argument ("range", metavar="MIN-MAX", nargs="?", help="two odd numbers with dash between them representing minimum and maximum value of the range to scan")
         
-        parser.add_argument ("output_file", metavar="OUTPUT_FILE", nargs="?", default="velvet_report.csv", help = "Name of the output file in csv format")
+        # argparse can't handle empty strings
+        if len(args) < 1: raise argparse.ArgumentTypeError ("Read help")
+                                         
+        parser.add_argument ("-i", "--input", metavar="INPUT_FOLDER", nargs="?", default=os.getcwd(), help = "Path to the folder where assemblies are located")
+                
+        parser.add_argument ("range", metavar="MIN-MAX", nargs="?", type=range_numbers, help="two odd numbers with dash between them representing minimum and maximum value of the range to scan")
+        
+        parser.add_argument ("-o", "--output", metavar="OUTPUT_FILE", nargs="?", default="velvet_report_MIN-MAX.csv", help = "Name of the output file in csv format.")
 
         
-        parser.add_argument ("-o", "--output", metavar="OUTPUT_FOLDER", default=os.path.join (os.getcwd()), help = "Path to the folder, where output file will be created") 
+        parser.add_argument ("-f", "--folder", metavar="OUTPUT_FOLDER", default=os.path.join (os.getcwd()), help = "Path to the folder, where output file will be created. Default is the folder you called a script from.") 
+
         
         result = parser.parse_args(args)
         # Finishing 
-        if result.output_file == 'velvet_report.csv': 
-                result.output_file = 'velvet_report' + "_" + result.range + ".csv"
-        result.range = [int(x) for x in result.range.split("-")]
+        if result.output == 'velvet_report_MIN-MAX.csv': 
+                result.output = 'velvet_report' + "_" + str(result.range[0]) + "-" + str(result.range[1]) + ".csv"
         
         
         return result
         
 
-def writeReports (writer, input):
-        for i in range (input['r'][0], input['r'][1], 2):
-                path = os.path.join (os.getcwd(), input['i'], str(i), "Log")
+def writeReports (input_folder, nRange, oWriter):
+        for i in range (nRange[0], nRange[1], 2):
+                path = os.path.join (input_folder, str(i), "Log")
                 print ("Trying to open file", path)
                 logfile = open(path, 'r')
                 log = logfile.read().split ("\n")        
@@ -123,16 +140,20 @@ def writeReports (writer, input):
                                 lociLine = l.split (" ")
                                 for j in range (len(lociLine)):
                                         if lociLine[j] == 'on': o.append (int (lociLine[j+1]))
-                writer.writerow (o)	
-                logfile.close()        
+                oWriter.writerow (o)	
+                logfile.close()  
+
+def prepareTemplate (path):
+        """ Open csv file and create headers """
+        output = open (path, 'w')
+        oWriter = csv.writer (output)
+        oWriter.writerow (['kmer', 'nodes', 'n50', 'max', 'total', 'loki'])
+        return (output, oWriter)
 
 def main (argv):
         input = input_handler (argv)
-        
-        output = open (os.path.join (os.getcwd(), input['o'], input['f']), 'w')
-        oWriter = csv.writer (output)
-        oWriter.writerow (['kmer', 'nodes', 'n50', 'max', 'total', 'loki'])
-        writeReports (oWriter, input)
+        output, oWriter = prepareTemplate (os.path.join(os.folder, os.output))
+        writeReports (input.input, input.range, oWriter)
         output.close()
 
 if __name__== "__main__":
--- tests/_combine_velvet_reports_test.py
+++ tests/_combine_velvet_reports_test.py
@@ -3,32 +3,53 @@ __author__ = 'Anton Shekhov'
 import unittest
 import sys
 import os
+import csv
+import argparse
 
 sys.path.append  (os.path.abspath("./"))
 sys.path.append  (os.path.abspath("../"))
 from src import combine_velvet_reports as v
 
 #TODO: make more tests before further development
-class VelvetTestCase (unittest.TestCase):
-        def testInputErrors (self):
-                pass
+#TODO: Create test for writeReports method
+class VelvetTestCase (unittest.TestCase):   
+
+        def setUp (self):
+                self.i_path = os.getcwd ()
+                self.o_path = os.getcwd ()
+                self.range_input = "1-5"
+                self.range_res = [1,5]
+                self.file_name = "velvet_report_1-5.csv"
+                self.input_path = os.path.join (self.i_path, self.file_name)
+                self.headers = ['kmer', 'nodes', 'n50', 'max', 'total', 'loki']
+
+        def testTemplate (self):
+                """ Check the headers for template creation """
+                #input = v.input_handler (["-i", self.i_path, self.range_input, "-f", self.o_path])
+                # Create file
+                output, oWriter = v.prepareTemplate (path=self.input_path)
+                output.close ()
+                # Open this file
+                test = open (self.input_path, 'r')
+                reader = csv.reader (test)
+                # Extract headers
+                headers = reader.__next__()
+                # Test it
+                self.assertEqual (headers, self.headers)
                 
+                # close file and remove
+                test.close()
+                os.remove (self.input_path)
+       
         def testRightInput (self):
-                i_path = os.getcwd()
-                o_path = os.getcwd()
-                r_test = [1,5]
-                #r_test = "1-5"
-                f_test = "velvet_report_1-5.csv"
-                test_result = v.input_handler ([i_path, "1-5", "-o", o_path]) 
-                self.assertEqual (i_path, test_result.input)
-                self.assertEqual (f_test, test_result.output_file)
-                self.assertEqual (o_path, test_result.output)
-                #self.assertEqual (f_test, test_result['f'])
-                self.assertEqual (r_test, test_result.range)
-                
-        def testWritReport (self):
-                pass
-                
+                """ Test the argument parser """
+                test_result = v.input_handler (["-i", self.i_path, self.range_input, "-f", self.o_path]) 
+                self.assertEqual (self.i_path, test_result.input)
+                self.assertEqual (self.file_name, test_result.output)
+                self.assertEqual (self.o_path, test_result.folder)
+                self.assertEqual (self.range_res, test_result.range)
+        
+
 
 if __name__ == '__main__':
         unittest.main()
